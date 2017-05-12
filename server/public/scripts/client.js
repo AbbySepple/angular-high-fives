0801c2558538d24948c5e813b24a9085eb44505b
Abby Sepple <abby.sepple@gmail.com>
2017-05-12 08:28:25 -0500

    working on buttong

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,11 +8,28 @@ myApp.controller('HighFiveController', function(GetNumber){
   // var ranNum = 0;
 
 
-
   vm.highfive = function() {
     console.log('inside highfive');
-
   vm.ranNum = GetNumber.randomNumber(1, 10);
-};
+};//end jake's high five
+
+vm.highsix = function() {
+  console.log('inside highfive');
+vm.ranNum = GetNumber.randomNumber(6, 10);
+};//end finns's high five
+
+vm.highfour = function() {
+  console.log('inside highfive');
+vm.ranNum = GetNumber.randomNumber(4, 10);
+};//end lady's high five
+
+vm.maybeHighFive = function(){
+  console.log('inside maybeHighFive');
+  if (vm.highfive >= vm.highfive){
+    console.log('High Five Man! ', vm.highfive);
+  } else {
+    console.log('Too Bad Bro, Try Again. ',vm.highfive );
+  }
+};//end maybeHighFive
 
 });//end myApp.controller
