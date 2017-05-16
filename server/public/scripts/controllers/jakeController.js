417a4d2dbbe56f7d786f28f563049959ac41148e
Abby Sepple <abby.sepple@gmail.com>
2017-05-16 17:10:55 -0500

    everything is broken

--- server/public/scripts/controllers/jakeController.js
+++ server/public/scripts/controllers/jakeController.js
@@ -1,4 +1,7 @@
-myApp.controller('jakeController', ['GetNumber', 'OooNumber'], function(GetNumber, OooNumber) {
+myApp.controller('jakeController', ['GetNumber', 'OooNumber', 'CompareHighFive', function(GetNumber, OooNumber, CompareHighFive) {
   console.log('Made it to the jakeController');
+  OooNumber.highfive();
+  CompareHighFive.ranNumUser(6, 10);
 
-});
+
+}]);
--- server/public/scripts/service.js
+++ server/public/scripts/service.js
@@ -9,10 +9,29 @@ myApp.service('GetNumber', function(){
 });//end myApp
 
 
-myApp.service('OooNumber', function(){
+myApp.service('OooNumber', ['GetNumber', function(GetNumber){
   var vm = this;
   vm.highfive = function() {
     vm.ranNum = GetNumber.randomNumber(1, 10);
     console.log('inside highfive:', vm.ranNum);
   };
-}); //end OooNumber
+}]); //end OooNumber
+
+
+myApp.service('CompareHighFive', ['GetNumber', function(GetNumber){
+  // var vm = this;
+// vm.highfiveUser = function(min, max) {
+  this.ranNumUser = GetNumber.randomNumber;
+
+  console.log('inside compare highfive', this.ranNumUser);
+  // vm.maybeHighFive = function() {
+    console.log('inside maybeHighFive');
+    if (this.ranNumUser >= this.ranNum) {
+      this.count++;
+      console.log('High Five Man! ');
+    } else {
+      console.log('Too Bad Bro, Try Again. ');
+    }
+// };
+
+}]);//end compare highfive
